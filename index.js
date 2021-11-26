1d25739e3204726c92ebec1262e717f1d51c0c19
Komla Adzam <adzamkomla.dev@gmail.com>
2021-11-26 18:33:40 +0000

    Add Store a Genre route

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const Joi = require("joi");
 const express = require("express");
 
 const app = express();
+
+app.use(express.json());
 
 const genres = [
   { id: 1, name: "Action" },
@@ -9,16 +12,38 @@ const genres = [
 
 app.get("/api/genres", (req, res) => res.send(genres));
 
+app.post("/api/genres", (req, res) => {
+  const { error } = validateBody(req.body);
+
+  if (error) return res.status(422).send(error.details[0].message);
+
+  if (
+    genres.find((g) => g.name.toLowerCase() === req.body.name?.toLowerCase())
+  ) {
+    return res.status(400).send("Genre already exists");
+  }
+
+  const genre = { id: genres.length + 1, name: req.body.name };
+  genres.push(genre);
+
+  res.send(genre);
+});
+
 app.get("/api/genres/:id", (req, res) => {
   const genre = genres.find((g) => g.id === +req.params.id);
 
   if (!genre) {
     return res.status(404).send("Genre not found");
   }
-  
+
   res.send(genre);
 });
 
-const port = process.env.PORT || 3000;
+const validateBody = (body) => {
+  return Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+  })?.validate(body);
+};
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Vidly listening on port ${port}!`));
