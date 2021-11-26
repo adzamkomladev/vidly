617b60ed02125c03476d5ae0f43916b80f476d95
Komla Adzam <adzamkomla.dev@gmail.com>
2021-11-26 18:43:21 +0000

    Add Update Genre route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-const genres = [
+let genres = [
   { id: 1, name: "Action" },
   { id: 2, name: "Comedy" },
 ];
@@ -26,7 +26,7 @@ app.post("/api/genres", (req, res) => {
   const genre = { id: genres.length + 1, name: req.body.name };
   genres.push(genre);
 
-  res.send(genre);
+  res.status(201).send(genre);
 });
 
 app.get("/api/genres/:id", (req, res) => {
@@ -37,6 +37,27 @@ app.get("/api/genres/:id", (req, res) => {
   }
 
   res.send(genre);
+});
+
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === +req.params.id);
+
+  if (!genre) {
+    return res.status(404).send("Genre not found");
+  }
+
+  const { error } = validateBody(req.body);
+
+  if (error) return res.status(422).send(error.details[0].message);
+
+  genres = genres.map((g) => {
+    if (g.id === genre.id) {
+      return { ...g, name: req.body.name };
+    }
+    return g;
+  });
+
+  res.status(204).send("Genre updated successfully");
 });
 
 const validateBody = (body) => {
