2f92abfe90a1f0b9bc37281f416259c794979165
Komla Adzam <adzamkomla.dev@gmail.com>
2021-11-26 18:01:24 +0000

    Add get Genre route

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const genres = [
 
 app.get("/api/genres", (req, res) => res.send(genres));
 
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === +req.params.id);
+
+  if (!genre) {
+    return res.status(404).send("Genre not found");
+  }
+  
+  res.send(genre);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Vidly listening on port ${port}!`));
