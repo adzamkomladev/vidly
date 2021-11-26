3ad082ca89e3cf5097d384875cafe59aa69c5dcc
Komla Adzam <adzamkomla.dev@gmail.com>
2021-11-26 17:58:17 +0000

    Add get all genres

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Comedy" },
+];
+
+app.get("/api/genres", (req, res) => res.send(genres));
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Vidly listening on port ${port}!`));
